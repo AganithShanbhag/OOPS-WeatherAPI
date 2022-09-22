458e8f16db12f9bcfb621515f41a608161f30148
Aganith Shanbhag <78319367+AganithShanbhag@users.noreply.github.com>
2022-09-22 19:58:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,18 +5,13 @@ Task phase 1 - complete the inline-functions (class functions) -- they are just 
 
 
 #include<bits/stdc++.h>
+#include <fstream>
 using namespace std;
+
 
 class weatherReport{
 
-    char placeName[50];
-    
-    //location format 
-    //decimal degrees DD, minutes MM, seconds SS
-    char DD[5];
-    char MM[5];
-    char SS[5];
-
+    protected:
     //weather parameters
     float AirTemperature;
     int HeatIndex;
@@ -33,22 +28,121 @@ class weatherReport{
     //define a construtor that and initialize with whatever seems fit {  }
 
 
-    //function to display like a table Detailed summary - everything{   }
+    //Reading the string and extracting data from it
+    void readTheWeather(string str){
+        //use regex
+    }
 
-    //function to display only what the user wants- ask what the user wants{   }
+
+    //function to display like a table Detailed summary - everything{   }
 
     //function to calculate something from already availabe data
     //I'm just typing calculate heat indrex{   }
 
     //function to convert the data in units demanded by the user - ex . mph to m/s {  }
 
+};
+
+
+class placeDetails : public weatherReport{
+
+    
+    string placeName;
+    //location format 
+    //decimal degrees DD, minutes MM, seconds SS
+    string coordinates;
+
+    public:
+
+    void inputPlaceDetails(string place, string coord){
+        placeName = place;
+        coordinates = coord;
+    }
+
+    void printDetails(){
+
+        cout<<placeName<<"\n";
+        cout<<coordinates<<"\n\n";
+
+    }
+
+    void detailedSummary(){
+        cout<<"\t\t\t\t---Complete summary of the weather ---\n\n";
+
+        cout<<"\nLoaction : "<< placeName;
+        cout<<"\nCoordinates: "<<coordinates;
+        cout<<"\n----------\n";
+        cout<<"\n";
+        cout<<"\nAir-Temperature: "<<AirTemperature;
+        cout<<"\nHumidity: "<<Humidity;
+        cout<<"\nPrecipitation: "<<Precipitation;
+    }
+
+    //function to display only what the user wants- ask what the user wants{   }
+
+        void customdisplay(){
+        cout<<"\t\t\t\t---What are thre paramaters you are interested in ? ---\n\n";
+
+        cout<<"Enter the corresponding number to view them";
+        string choice;
+
+        cout<<"Yet to build";
+        }
+    
+
+
+    string getPlaceName(){
+        return placeName;
+    }
 
 
 };
 
+
+void getPlacenames(string & placeNames){
+    ifstream data("data/places.txt");
+    getline(data, placeNames);
+}
+
+
 int main(){
 
     cout<<"WeatherAPI with file streams";
+
+    //read the file with info about the cities/places and dump it into an array of string
+
+    string placeNames;
+    getPlacenames(placeNames);    
+    
+    istringstream ss(placeNames);
+ 
+    string place; // for storing each word
+ 
+    // Traverse through all words
+    // while loop till we get
+    // strings to store in string word
+    vector< placeDetails *> ptr;
+    while (ss >> place)
+    {
+    placeDetails * location = new placeDetails;
+    ptr.push_back(location);
+
+    location->inputPlaceDetails(place, "10deg 44mins 3sec");
+    }
+
+    for(int i = 0; i < ptr.size(); i++){
+        ptr[i]->printDetails();
+        string location = "data/weather-data" + ptr[i]->getPlaceName();
+        ifstream locationWeather(location);
+        string data;
+        getline(locationWeather, data);
+        ptr[i]->readTheWeather(data);
+    }
+    //By now u will have an object for every location containing its seperate copy of weather data
+
+
+
+
 
 //we will read the file (text or DAT file then dump it into the class object)
 
